459db0477ea48751f3160672fb9f30cef12fa92b
Pavel Dronov <dronov@adn.agency>
2022-06-22 21:59:00 +0700

    default layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='page'>
+      <header className="header">
+        <div className="layout">
+          <img className='logo' src="https://image.winudf.com/v2/image/aW8uc21hbGxkYXRhbGFiLmFuZHJvaWQuY29udGV4dF9pY29uXzBfYzE3ZDkyZWM/icon.png?w=340&fakeurl=1" alt=''/>
+        </div>
       </header>
+      <div className="layout">
+        <div className="page__wrapper">
+          <div className="sidebar">
+            <nav className='menu'>
+              <ul>
+                <li>
+                  <a href="#s">Feed</a>
+                </li>
+                <li>
+                  <a href="#S">Message</a>
+                </li>
+                <li>
+                  <a href="#s">Music</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="body"></div>
+        </div>
+      </div>
+
     </div>
   );
 }
